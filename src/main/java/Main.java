d38ceae22b0ad97d93e74f84321ac6fa2c38d72e
vandhanavvishnu <vandhana.vishnu@tigeranalytics.com>
2024-12-16 04:47:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+   public class Main {
+        public static void main(String[] args) {
+            //code will go in here
+        }
+    }
+
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
